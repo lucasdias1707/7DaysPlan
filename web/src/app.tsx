fac6fd486447dd41831088d999c3107215f9bb35
lucasdias1707 <lucas.dortiz17@gmail.com>
2025-02-10 23:51:21 -0300

    finalizado aula

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { CreateGoal } from "./components/create-goal";
+import { EmptyGoals } from "./components/empty-goals";
+import { Summary } from "./components/summary";
+import { Dialog } from "./components/ui/dialog";
+import { getSummary } from "./http/get-summary";
+
 export function App() {
-  return <h1 className="bg-black text-white text-xl">Teste</h1>;
+  const { data } = useQuery({
+    queryKey: ["summary"],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 seconds
+  });
+
+  return (
+    <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
+      <CreateGoal />
+    </Dialog>
+  );
 }
