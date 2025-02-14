306ce5f24fd08ded197bcc4c6aeac3e51f514135
lucasdias1707 <lucas.dortiz17@gmail.com>
2025-02-13 22:15:38 -0300

    Instalado o sonner e iniciado o toast

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "sonner";
 import { App } from "./app";
 
 import "./index.css";
@@ -10,6 +11,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster position="top-left" />
       <App />
     </QueryClientProvider>
   </StrictMode>
